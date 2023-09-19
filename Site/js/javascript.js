7499f74130ce3ecf46dcefc39a5096d71a906ff0
Filipe297 <filipemb400@gmail.com>
2023-09-19 13:23:01 -0300

    javascript

--- Site/js/javascript.js
+++ Site/js/javascript.js
@@ -4,3 +4,32 @@ console.log("# Javascript está carregado...");
 $(function(){
     console.log("# jQuery(v3.1.1) está carregado também...");
 })
+
+
+// Código para ativar o slide
+$('').slick({
+    dots: false,
+    arrows:true,
+    speed:1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover:true,
+    responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        },
+                    },
+                    {
+                        breakpoint: 568,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        },
+                    }
+                ],
+});$('.slick-arrow:nth-of-type(1)').html("<");$('.slick-arrow:nth-of-type(2)').html(">");
