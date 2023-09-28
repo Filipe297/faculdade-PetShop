1a66a523c8c35c2f4c6599f98443f9cb1f6c9e53
Filipe297 <filipemb400@gmail.com>
2023-09-28 16:14:39 -0300

    update php

--- Site/config.php
+++ Site/config.php
@@ -1,9 +1,21 @@
 <?php
 //configurações e funções do php
-echo "esse é o arquivo php de configurações e funções gerais<br>";
 
-include("php/classes.php");
-$painel = New Site();
+    session_start();
+	date_default_timezone_set('America/Sao_Paulo');
+    define('INCLUDE_PATH','');
+    define('BASE_DIR_PAINEL','');
+
+    $css = '<link href="<?php echo INCLUDE_PATH; ?>CSS/style.css" rel="stylesheet" />';
+
+    //Conectar com banco de dados!
+	define('HOST','');
+	define('USER','');
+	define('PASSWORD','');
+	define('DATABASE',''); //mudar
+
+    include("php/classes.php");$painel = New Site();
+    
 
 
 ?>
